55a58f151048d77933ae76744fc1d1a1fcd037fb
taurus79 <Henry@Henryui-MacBook-Pro.local>
2015-07-10 21:29:03 -0500

    change a couple of comments in R codes

--- code and plots/Plot4.R
+++ code and plots/Plot4.R
@@ -6,7 +6,7 @@ Plot4 <- function() {
   data <- read.table("household_power_consumption.txt", sep=";", header=TRUE, 
                      colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings="?")
   
-  ## subset data for 2007-02-01 and 2007-02-02 from originally imported data  
+  ## subset data for the date of 2007-02-01 and 2007-02-02 from the originally imported data  
   data.f <- filter(data, Date =='1/2/2007' | Date =='2/2/2007')
   
   ## combine date and time strings and then convert it as POSIXlt format using strptime function 
